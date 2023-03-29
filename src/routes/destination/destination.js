e245740947d1896375ba12b25f6c578cf4b88a61
Nicolas Herbreteau <nicolas.herbreteau@gmail.com>
2023-03-29 15:53:10 +0100

    displaying data and adding the database

--- src/routes/destination/destination.js
+++ src/routes/destination/destination.js
@@ -1,17 +1,29 @@
 import backgroundDestination from "../../assets/destination/background-destination-desktop.jpg"
-import {VariantBackground} from "./styles";
+import {VariantBackground, NavDestination, RightContainer, NavLink} from "./styles";
 import {motion} from "framer-motion";
+import {useState} from "react";
+import getData from "./getData";
 
 
 const Destination = () => {
+    const [content, setContent] = useState('moon')
+    const {data} = getData(content)
     return (
         <>
+        <RightContainer>
+            <NavDestination>
+                <NavLink onClick={() => {setContent('moon')}}>Moon</NavLink>
+                <NavLink onClick={() => {setContent('mars')}}>Mars</NavLink>
+            </NavDestination>
+        </RightContainer>
             <motion.div
                 variants={VariantBackground}
                 initial="initial"
                 animate="animate"
             >
+
                 <img src={backgroundDestination} style={VariantBackground} alt="background"/></motion.div>
+
         </>
     )
 }
--- src/routes/destination/styles.js
+++ src/routes/destination/styles.js
@@ -15,3 +15,38 @@ export const VariantBackground = {
         },
     },
 }
+
+export const NavDestination = styled.div `
+    position: absolute;
+    left: 0%;
+    right: 35.84%;
+    top: 0%;
+    bottom: 92.8%;
+    display: flex;
+    flex-direction: row
+`
+
+export const RightContainer = styled.div `
+    position: absolute;
+    width: 445px;
+    height: 472px;
+    right: 30px;
+    top: 250px;
+`
+
+export const NavLink = styled.button `
+    font-family: Barlow Condensed;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 19px;
+    letter-spacing: 2.700000047683716px;
+    text-align: left;
+    font-family: 'Barlow Condensed';
+    text-transform: uppercase;
+    color: #FFFFFF;
+    background: none;
+    border: none;
+    &:hover {
+    cursor: pointer;
+    }
+`
